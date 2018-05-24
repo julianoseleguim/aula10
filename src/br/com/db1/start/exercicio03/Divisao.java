dcbe7c2293e1cb511b14525a4eee33b4cf97d20d
julianoseleguim <jseleguim@gmail.com>
2018-05-24 10:04:32 -0300

    Exercicio 03

---
+++ src/br/com/db1/start/exercicio03/Divisao.java
@@ -0,0 +1,11 @@
+package br.com.db1.start.exercicio03;
+
+public class Divisao implements OperacaoMatematica {
+
+	@Override
+	public double calcula(Double valor1, Double valor2) {
+		// TODO Auto-generated method stub
+		return valor1/valor2;
+	}
+
+}
---
+++ src/br/com/db1/start/exercicio03/OperacaoMatematica.java
@@ -0,0 +1,6 @@
+package br.com.db1.start.exercicio03;
+
+public interface OperacaoMatematica {
+	
+	double calcula(Double valor1, Double valor2);
+}
