da05b675e53738814cadf7d9137d2354f29dba34
Aluno <Aluno>
2018-05-24 14:08:26 -0300

    Exercicio 06

---
+++ src/br/com/db1/start/exercicio06/Pedido.java
@@ -0,0 +1,42 @@
+package br.com.db1.start.exercicio06;
+
+import java.util.Date;
+
+public class Pedido {
+
+	private Integer numero;
+	private Integer numeroMesa;
+	private Date dataPedido;
+	
+	public void cadastratPedido(){		
+	}
+	
+	public void fechaPedido(){
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Integer getNumeroMesa() {
+		return numeroMesa;
+	}
+
+	public void setNumeroMesa(Integer numeroMesa) {
+		this.numeroMesa = numeroMesa;
+	}
+
+	public Date getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(Date dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+	
+	
+}
